9de5b366484a5021949e2d7b3812b87d38b45a9f
Anna Kolmakova <annakolmakova@gmail.com>
2022-11-11 21:47:52 +0200

    All components created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('toggle').addEventListener('click', () => {
+    const textStarter = document.getElementById('price-starter');
+    const textProfessional = document.getElementById('price-professional');
+    const textCompany = document.getElementById('price-company');
+
+    if (
+      textStarter.innerHTML === '$19'
+      && textProfessional.innerHTML === '$54'
+      && textCompany.innerHTML === '$89'
+    ) {
+      textStarter.innerHTML = '$12';
+      textProfessional.innerHTML = '$36';
+      textCompany.innerHTML = '$56';
+    } else {
+      textStarter.innerHTML = '$19';
+      textProfessional.innerHTML = '$54';
+      textCompany.innerHTML = '$89';
+    }
+  });
+});
