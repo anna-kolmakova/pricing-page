7a3f36b9fa266ba7f9d9114a64cdda1492155552
Anna Kolmakova <annakolmakova@gmail.com>
2022-11-13 16:52:19 +0200

    Price data variables added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,19 +5,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const textStarter = document.getElementById('price-starter');
     const textProfessional = document.getElementById('price-professional');
     const textCompany = document.getElementById('price-company');
+    const priceStarterMonthly = '$19';
+    const priceStarterYearly = '$12';
+    const priceProfessionalMonthly = '$54';
+    const priceProfessionalYearly = '$36';
+    const priceCompanyMonthly = '$89';
+    const priceCompanyYearly = '$56';
 
     if (
-      textStarter.innerHTML === '$19'
-      && textProfessional.innerHTML === '$54'
-      && textCompany.innerHTML === '$89'
+      textStarter.innerHTML === priceStarterMonthly
+      && textProfessional.innerHTML === priceProfessionalMonthly
+      && textCompany.innerHTML === priceCompanyMonthly
     ) {
-      textStarter.innerHTML = '$12';
-      textProfessional.innerHTML = '$36';
-      textCompany.innerHTML = '$56';
+      textStarter.innerHTML = priceStarterYearly;
+      textProfessional.innerHTML = priceProfessionalYearly;
+      textCompany.innerHTML = priceCompanyYearly;
     } else {
-      textStarter.innerHTML = '$19';
-      textProfessional.innerHTML = '$54';
-      textCompany.innerHTML = '$89';
+      textStarter.innerHTML = priceStarterMonthly;
+      textProfessional.innerHTML = priceProfessionalMonthly;
+      textCompany.innerHTML = priceCompanyMonthly;
     }
   });
 });
